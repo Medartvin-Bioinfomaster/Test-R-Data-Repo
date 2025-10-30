5be3f2ffae8fd2c37265d67e39a81f57b440b0a1
bobauser <edvin.ulvestad@gmail.com>
2025-10-30 14:52:47 +0100

    added R files in a R folder

---
+++ R/calculator.R
@@ -0,0 +1,21 @@
+addition <- function(a, b) {
+    return(a + b)
+}
+
+subtraction <- function(a, b) {
+    return(a - b)
+}
+
+# so this is how it works:
+x <- 10
+y <- 5
+aVal <- addition(x, y)
+
+sVal <- subtraction(x, y)
+
+# TODO: add a multiplication function
+
+# TODO: add a division function
+
+print(aVal)
+print(sVal)
---
+++ R/helloworld.R
@@ -0,0 +1,3 @@
+print("Hello, World!")
+
+#* TODO: fix the print to say Hello Man
